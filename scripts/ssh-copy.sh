72a8d9cb222eb218bcc474db59a9af2ee0122032
sixsa <70724422+sixsa@users.noreply.github.com>
2023-07-04 17:13:10 -0400

    updated permission setting for ssh copy

--- scripts/ssh-copy.sh
+++ scripts/ssh-copy.sh
@@ -9,11 +9,25 @@ if [$WINDOWS_USERNAME == ""]; then
     WINDOWS_USERNAME=$USER
 fi
 
+mkdir -p $HOME/.ssh/
+sudo chmod 700 $HOME/.ssh/
+echo "set $HOME/.ssh/ permissions to 700" ;;
+
 if [ -d "/mnt/c/Users/$WINDOWS_USERNAME/.ssh" ]; then
     cp -a /mnt/c/Users/$WINDOWS_USERNAME/.ssh/. $HOME/.ssh/
-    # Update permissions
-    for FILE in $HOME/.ssh/; do 
-        sudo chmod 600 $FILE
+    # set correct permissions (ttps://meng6.net/pages/blog/permission_of_.ssh_files/)
+    for FILE in $HOME/.ssh/*; do
+        case $FILE in
+            (*.pub)
+                sudo chmod 644 $FILE
+                echo "set $FILE permissions to 644" ;;
+            (*known_hosts)
+                sudo chmod 644 $FILE
+                echo "set $FILE permissions to 644" ;;
+            (*)
+                sudo chmod 600 $FILE
+                echo "set $FILE permissions to 600" ;;
+        esac
     done
 else
     echo "failure: could not find Windows .ssh folder"
