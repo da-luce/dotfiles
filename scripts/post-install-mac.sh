6b22ef677b9363930dd4e61b2f8e7d45f07a58bb
Dalton Luce <sixsa@macbook-air.myfiosgateway.com>
2023-08-10 18:39:41 -0400

    minor updates

--- scripts/post-install-mac.sh
+++ scripts/post-install-mac.sh
@@ -1,6 +1,4 @@
-# Install Homebrew and add to path
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile
+# Install programs using Homebrew
 
 # Make sure we're all set and update
 brew doctor
@@ -9,9 +7,20 @@ brew tap homebrew/cask-versions
 # Dev
 brew install starship
 brew install autojump
+
+brew install lua
+brew install tree-sitter
+brew install luajit
+brew install neovim
+
+# Neovim package manager
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 brew install --cask multipass
 brew install --cask wezterm
 brew install --cask visual-studio-code
+brew install --cask xquartz
 
 # Social media
 brew install --cask slack
@@ -25,7 +34,3 @@ brew install --cask bitwarden
 brew install --cask google-drive
 brew install --cask microsoft-office
 brew install --cask firefox-developer-edition
-
-# Update path
-echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile
-echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile
